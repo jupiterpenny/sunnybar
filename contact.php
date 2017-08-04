22c729c6e2fd9b4450980f3c85a70e889c4a6101
Amanda Krummel <amandakrummel@Amandas-MacBook-Pro.local>
2017-08-04 14:52:27 -0400

    commit bar

---
+++ contact.php
@@ -0,0 +1,42 @@
+<?php 
+
+
+
+
+
+if(isset($_POST['submit'])){
+
+
+  $to = "daytonatowers@cfl.rr.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $name = $_POST['name'];
+    $subject = "Form submission";
+    $subject2 = "This is a copy of the email you sent DBOT";
+    $message = $name . " " . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['message'];
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+ 
+
+
+}
+
+
+?>
+<html>
+
+<a href="index.html"><button>HOME</button></a>
+</html>
+
+<?PHP
+    
+    
+?>
+    
+    
+    
+
